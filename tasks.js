73448917dcb122202a084230d814986124e64b49
Syed Furkhan <you@your-domain.com>
2026-02-23 20:33:44 +0530

    feat: add friendly message

--- tasks.js
+++ tasks.js
@@ -21,7 +21,7 @@ function addTask(taskName) {
     
     // FIXED: Changed 'pussh' back to 'push'
     myTasks.push(newTask);
-    console.log(`🚀 New Task Created: "${taskName}"`);
+    console.log("✨ Sweet! New task added: " + taskName);
 }
 
 /**
